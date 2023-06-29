b1a8182333cd8b2843827e6680947097f50889ca
jv1111 <webd0829@gmail.com>
2023-06-29 10:12:03 +0800

    Log.i (print with tags)

--- app/src/main/java/com/example/d1kotlin/MainActivity.kt
+++ app/src/main/java/com/example/d1kotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.d1kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MYTAG","Main activity on create");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MYTAG","Main activity destroyed");
     }
 }
